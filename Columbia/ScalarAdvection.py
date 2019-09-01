adcdafd7a6f5ff48a1e5304de2fb586ac4282e42
Kyle Pressel <pressel@gmail.com>
2019-08-31 23:24:31 -0700

    Added general structure for ScalarAdvection.

---
+++ Columbia/ScalarAdvection.py
@@ -0,0 +1,24 @@
+def ScalarAdvectionFactory(namelist): 
+
+    return 
+
+
+class ScalarAdvectionBase: 
+
+    def __init__(self): 
+
+        return 
+
+    def update(self): 
+
+        return 
+
+
+class WENO_5th_Order(ScalarAdvectionBase): 
+    def __init__(self): 
+        ScalarAdvectionBase.__init__(self) 
+        return 
+
+    def update(); 
+
+        return 
