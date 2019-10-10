Fixed bug that breaks parallel scaling.

Columbia/Grid.py
@@ -54,6 +54,10 @@ class GridBase:
         return self._ngrid
 
     @property
+    def ngrid_local(self):
+        return self._ngrid_local
+
+    @property
     def dx(self):
         return self._dx
 
@@ -158,7 +162,8 @@ class GridBase:
         self._local_start = np.array(dum_array.substart)
         self._local_end = self._local_start + self._local_shape
 
-
+        #Get the local shape
+        self._ngrid_local = self._local_shape + 2 * self._n_halo
 
 
         return

Columbia/ParallelArrays.py
@@ -13,7 +13,7 @@ class GhostArray(GhostArrayBase):
     def __init__(self, _Grid, dtype=np.double,ndof=1): 
 
         GhostArrayBase.__init__(self, _Grid)
-        self._shape = tuple(np.append(ndof, self._Grid.ngrid))
+        self._shape = tuple(np.append(ndof, self._Grid.ngrid_local))
         self._n_halo = self._Grid.n_halo 
         self.array = np.empty(self._shape, dtype=np.double)  
 