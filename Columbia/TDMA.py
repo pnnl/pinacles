59b97b1f80977e7a1f489bfc2e5a4f735c68585d
Kyle Pressel <pressel@gmail.com>
2019-10-11 16:53:13 -0700

    TDMA solver passing unit test.

--- Columbia/TDMA.py
+++ Columbia/TDMA.py
@@ -5,16 +5,16 @@ import numba
 def Thomas(x, a, b, c):
     shape = x.shape
     scratch = np.empty(shape[2], dtype=np.double)
-    scratch[0] = c[0]/b[0]
-    x[0] = x[0]/b[0]
-
     for i in range(shape[0]):
         for j in range(shape[1]):
+            #Upward sweep
+            scratch[0] = c[0]/b[0]
+            x[i,j,0] = x[i,j,0]/b[0]
             for k in range(1,shape[2]):
-                m = 1.0/(b[i] - a[i] * scratch[i-1])
-                scratch[i] = c[i] * m
-                x[i] = (x[i] - a[i] * x[i-1])*m
-
+                m = 1.0/(b[k] - a[k] * scratch[k-1])
+                scratch[k] = c[k] * m
+                x[i,j,k] = (x[i,j,k] - a[k] * x[i,j,k-1])*m
+            #Downward sweep
             for k in range(shape[2]-2,-1,-1):
-                x[i] = x[i] - scratch[i] * x[i+1]
+                x[i,j,k] = x[i,j,k] - scratch[k] * x[i,j,k+1]
     return
--- tests/test_TDMA.py
+++ tests/test_TDMA.py
@@ -8,9 +8,14 @@ def test_TDMA():
     c = np.array([-1, -1, -1, 0], dtype=np.double)
 
     rhs = np.empty((4,4,4), dtype=np.double)
-    rhs[:,:,:] = np.array([5, 5, 10, 23], dtype=np.double)[np.newaxis, np.newaxis, :]
+    rhs[:,:,:] = np.array([21, 69, 34, 22], dtype=np.double)[np.newaxis, np.newaxis, :]
 
     TDMA.Thomas(rhs, a, b, c)
 
+    a_test = np.array([[4, -1, 0, 0], [-1, 4, -1, 0], [0, -1, 4, -1], [0, 0, -1, 4] ], dtype=np.double)
+    b_test = np.array([21, 69, 34, 22], dtype=np.double)
+
+    test_soln = np.linalg.solve(a_test, b_test) 
+    assert(np.allclose(rhs[:,:,:], test_soln[np.newaxis, np.newaxis, :]))
 
     return
