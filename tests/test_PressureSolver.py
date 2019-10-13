6d3dfd636ec401365ff35bea86632589f4aba53a
Kyle Pressel <pressel@gmail.com>
2019-10-13 08:38:46 -0700

    Added tests for modified wave number.

--- tests/test_PressureSolver.py
+++ tests/test_PressureSolver.py
@@ -37,6 +37,26 @@ def build_mocks(n=[16, 16, 100]):
     return namelist, Ref, ModelGrid, ScalarState, VelocityState, DiagnosticState
 
 
+def test_modified_wavenumbers(): 
+    n = 16 
+    namelist, Ref, ModelGrid, ScalarState, VelocityState, DiagnosticState = build_mocks([n,n,n]) 
+    
+    #Since we are just checking the wave numbers, no need to init any fields
+    PresTest = PressureSolver.PressureSolver(ModelGrid, Ref, VelocityState)
+
+    #Check that the sizes are correct 
+    assert(np.shape(PresTest._kx2) == (16,))
+    assert(np.shape(PresTest._ky2) == (16,))
+
+    #The test domain is symmetric so check that kx2 equals ky2
+    assert(np.all(PresTest._kx2 == PresTest._ky2))
+
+    #Check symmetry
+    for i in range(int(n/2)): 
+        assert(PresTest._kx2[i] == PresTest._kx2[-i])
+        assert(PresTest._ky2[i] == PresTest._ky2[-i])
+
+    return 
 
 def test_divergence():
 
