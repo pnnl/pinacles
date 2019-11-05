a3f6db258fbcca083908ca7247f5c70875570d7e
Kyle Pressel <pressel@gmail.com>
2019-11-05 14:51:12 -0800

    Cleaned up TDMA tests.

--- tests/test_TDMA.py
+++ tests/test_TDMA.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 import Columbia.Grid as Grid
 import Columbia.Containers as Containers
 import Columbia.ReferenceState as ReferenceState
@@ -23,9 +23,9 @@ def test_TDMA():
 
     return
 
-def build_mocks(n=[16, 16, 100]): 
+def build_mocks(n=[16, 16, 100]):
     namelist = {}
-    namelist['grid'] = {} 
+    namelist['grid'] = {}
     namelist['grid']['n'] = n
     namelist['grid']['l'] = [1000.0, 1000.0, 1000.0]
     namelist['grid']['n_halo'] = [3, 3, 3]
@@ -33,8 +33,8 @@ def build_mocks(n=[16, 16, 100]):
     ModelGrid = Grid.RegularCartesian(namelist)
     ScalarState = Containers.ModelState(ModelGrid, prognostic=True)
     VelocityState = Containers.ModelState(ModelGrid, prognostic=True)
-    DiagnosticState = Containers.ModelState(ModelGrid)   
-    Ref = ReferenceState.ReferenceDry(namelist, ModelGrid) 
+    DiagnosticState = Containers.ModelState(ModelGrid)
+    Ref = ReferenceState.ReferenceDry(namelist, ModelGrid)
     Ref.set_surface()
     Ref.integrate()
 
@@ -52,29 +52,3 @@ def build_mocks(n=[16, 16, 100]):
     ScalarState.allocate()
 
     return namelist, Ref, ModelGrid, ScalarState, VelocityState, DiagnosticState
-
-
-def test_modified_wavenumbers():
-    n = 16
-    namelist, Ref, ModelGrid, ScalarState, VelocityState, DiagnosticState = build_mocks([n,n,n]) 
-
-    #Since we are just checking the wave numbers, no need to init any fields
-    PresTest = TDMA.PressureTDMA(ModelGrid, Ref)
-
-    #Check that the sizes are correct
-    assert(np.shape(PresTest._kx2) == (16,))
-    assert(np.shape(PresTest._ky2) == (16,))
-
-    #The test domain is symmetric so check that kx2 equals ky2
-    assert(np.all(PresTest._kx2 == PresTest._ky2))
-
-    #Check that we have removed the odd ball
-    assert(PresTest._kx2[0] == 0.0)
-    assert(PresTest._ky2[0] == 0.0)
-
-    #Check symmetry not included first and last element
-    for i in range(1,int(n/2)-1):
-        assert(PresTest._kx2[i] == PresTest._kx2[-i])
-        assert(PresTest._ky2[i] == PresTest._ky2[-i])
-
-    return 
