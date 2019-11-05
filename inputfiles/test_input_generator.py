585b549187c8b965209f437da793935a01976f25
Kyle Pressel <pressel@gmail.com>
2019-11-04 16:39:19 -0800

    Adding input generator.

---
+++ inputfiles/test_input_generator.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+
+def main(casename):
+    ''' The function generates input files that are necessary for testing.'''
+    if casename == 'colliding_blocks':
+        input_dict = colliding_blocks()
+
+    write_file(casename, input_dict)
+
+    return
+
+def write_file(casename, input_dict):
+    with open('./' + casename + '.json', 'w') as input_file_out:
+        json.dump(input_dict, input_file_out, sort_keys=True, indent=4)
+    return
+
+def colliding_blocks():
+    ''' data necessary for the simplest testing of the domain '''
+    input_dict = {}
+
+    key = 'meta'
+    input_dict[key] = {}
+    input_dict[key]['casename'] = 'colliding_blocks'
+
+    key = 'grid'
+    input_dict[key] = {}
+    #Set the number of grid points in the domain
+    input_dict[key]['n'] = [256, 256, 5]
+    #Set the number of halo points in each direct
+    input_dict[key]['n_halo'] = [1, 1, 1]
+    #Set the domain length, dx will be determined from n and L
+    input_dict[key]['l'] = [1000.0, 1000.0, 1000.0]
+
+    return input_dict
+
+LIST_OF_CASES = ['colliding_blocks']
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Aarg paraser for generating input files.')
+    parser.add_argument('casename')
+    args = parser.parse_args()
+
+    assert(args.casename in LIST_OF_CASES)
+
+    main(args.casename)
