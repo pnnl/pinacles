df4ec33296d752484bfd61664eab4e8c0ed968de
Kyle Pressel <pressel@gmail.com>
2019-11-06 14:02:39 -0800

    Adding files for damping.

---
+++ Columbia/Damping.py
@@ -0,0 +1,19 @@
+from Columbia import Damping_impl
+
+class Damping: 
+    def __init__(self, namelist): 
+
+        return 
+
+    def update(self): 
+
+        return
+
+class Rayleigh(Damping): 
+    def __init__(self, namelist):
+
+        return 
+
+    def update(self): 
+
+        return
---
+++ tests/test_Damping.py
@@ -0,0 +1,2 @@
+import Columbia.Damping as Damping
+
