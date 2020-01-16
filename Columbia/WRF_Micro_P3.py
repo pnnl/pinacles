5d5d9084417927baa4988181ce462c50912583de
Kyle Pressel <pressel@gmail.com>
2020-01-16 09:04:23 -0800

    Import of p3 mod now works.

---
+++ Columbia/WRF_Micro_P3.py
@@ -0,0 +1,7 @@
+from Columbia.Microphysics import MicrophysicsBase
+from Columbia.wrf_physics import p3
+from Columbia import UtilitiesParallel
+from Columbia import parameters
+from mpi4py import MPI
+import numba
+
