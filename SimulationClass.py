f086039c8a980d7dcf3a614b5142ba6ac225441e
Kyle Pressel <pressel@gmail.com>
2020-01-19 00:54:02 -0800

    Add ability to run multiple domains.

---
+++ SimulationClass.py
@@ -0,0 +1,152 @@
+import numba
+import json
+import argparse
+from Columbia import Initializaiton
+from Columbia import TerminalIO, Grid, ParallelArrays, Containers, Thermodynamics
+from Columbia import ScalarAdvection, TimeStepping, ReferenceState
+from Columbia import MomentumAdvection
+from Columbia import PressureSolver
+from Columbia import Damping
+from Columbia import SurfaceFactory
+from Columbia import ForcingFactory
+from Columbia.Stats import Stats
+from Columbia import DumpFields
+from Columbia import MicrophysicsFactory
+from mpi4py import MPI
+import numpy as np
+import time
+import pylab as plt
+import copy
+
+import os 
+os.environ["HDF5_USE_FILE_LOCKING"]="FALSE"
+
+class Simulation: 
+    def __init__(self, namelist, domain_number):
+        self._namelist = copy.deepcopy(namelist)
+        self._domain_number = domain_number
+    
+    
+    
+    def initialize(self):
+        namelist = self._namelist
+
+        self.ModelGrid = Grid.RegularCartesian(namelist)
+        self.ScalarState = Containers.ModelState(self.ModelGrid, container_name='ScalarState', prognostic=True)
+        self.VelocityState = Containers.ModelState(self.ModelGrid, container_name='VelocityState', prognostic=True)
+        self.DiagnosticState = Containers.ModelState(self.ModelGrid, container_name='DiagnosticState')
+        self.ScalarTimeStepping = TimeStepping.factory(namelist, self.ModelGrid, self.ScalarState)
+        self.VelocityTimeStepping = TimeStepping.factory(namelist, self.ModelGrid, self.VelocityState)
+        self.TimeSteppingController = TimeStepping.TimeSteppingController(namelist, self.ModelGrid, self.VelocityState)
+        self.RayleighDamping = Damping.RayleighInitial(namelist, self.ModelGrid)
+        self.RayleighDamping.add_state(self.VelocityState)
+        self.RayleighDamping.add_state(self.ScalarState)
+
+        self.TimeSteppingController.add_timestepper(self.ScalarTimeStepping)
+        self.TimeSteppingController.add_timestepper(self.VelocityTimeStepping)
+
+        self.Ref =  ReferenceState.factory(namelist, self.ModelGrid)
+       
+        # Add velocity variables
+        self.VelocityState.add_variable('u')
+        self.VelocityState.add_variable('v')
+        self.VelocityState.add_variable('w', loc='z', bcs='value zero')
+
+        # Set up the thermodynamics class
+        self.Thermo = Thermodynamics.factory(namelist, self.ModelGrid, self.Ref, self.ScalarState, self.VelocityState, self.DiagnosticState)
+
+        #Setup the scalar advection calss
+        self.ScalarAdv = ScalarAdvection.factory(namelist, self.ModelGrid, self.Ref, self.ScalarState, self.VelocityState)
+        self.MomAdv = MomentumAdvection.factory(namelist, self.ModelGrid, self.Ref, self.ScalarState, self.VelocityState)
+
+        #Setup the pressure solver
+        self.PSolver = PressureSolver.factory(namelist, self.ModelGrid, self.Ref, self.VelocityState, self.DiagnosticState)
+        self.Micro = MicrophysicsFactory.factory(namelist, self.ModelGrid, self.Ref, self.ScalarState, self.VelocityState, self.DiagnosticState, self.TimeSteppingController)
+
+        # Allocate all of the big parallel arrays needed for the container classes
+        self.ScalarState.allocate()
+        self.VelocityState.allocate()
+        self.DiagnosticState.allocate()
+
+        self.ScalarTimeStepping.initialize()
+        self.VelocityTimeStepping.initialize()
+
+        Initializaiton.initialize(namelist, self.ModelGrid, self.Ref, self.ScalarState, self.VelocityState)
+
+        self.RayleighDamping.init_means()
+        self.Surf = SurfaceFactory.factory(namelist, self.ModelGrid, self.Ref, self.VelocityState, self.ScalarState, self.DiagnosticState)
+        self.Force = ForcingFactory.factory(namelist, self.ModelGrid, self.Ref, self.VelocityState, self.ScalarState, self.DiagnosticState)
+        self.PSolver.initialize() #Must be called after reference profile is integrated
+
+        #Setup Stats-IO
+        self.StatsIO = Stats(namelist, self.ModelGrid, self.Ref, self.TimeSteppingController)
+
+        self.StatsIO.add_class(self.VelocityState)
+        self.StatsIO.add_class(self.ScalarState)
+        self.StatsIO.add_class(self.DiagnosticState)
+        self.StatsIO.add_class(self.Micro)
+
+
+        self.FieldsIO = DumpFields.DumpFields(namelist, self.ModelGrid, self.TimeSteppingController)
+        self.FieldsIO.add_class(self.ScalarState)
+        self.FieldsIO.add_class(self.VelocityState)
+        self.FieldsIO.add_class(self.DiagnosticState)
+
+
+        self.StatsIO.initialize()
+
+
+        self.ScalarState.boundary_exchange()
+        self.VelocityState.boundary_exchange()
+        self.ScalarState.update_all_bcs()
+        self.VelocityState.update_all_bcs()
+        self.PSolver.update()
+
+    def update(self, timestop, ls_forcing):
+        while self.TimeSteppingController.time < timestop:
+            for n in range(self.ScalarTimeStepping.n_rk_step):
+                self.TimeSteppingController.adjust_timestep(n)
+
+                #Update Thermodynamics
+                self.Thermo.update()
+
+                #Do StatsIO if it is time
+                if n == 0:
+                    self.StatsIO.update()
+                    MPI.COMM_WORLD.barrier()
+
+                #Update microphysics
+                self.Micro.update()
+
+                #Update the surface
+                self.Surf.update()
+
+                #Update the forcing
+                self.Force.update()
+
+                #Update scalar advection
+                self.ScalarAdv.update()
+                self.MomAdv.update()
+
+                #Do Damping
+                self.RayleighDamping.update()
+
+                #Apply large scale forcing
+                for v in ls_forcing:
+                    self.ScalarState.get_tend(v)[:,:,:] += ls_forcing[v][np.newaxis,np.newaxis,:] 
+
+                #Do time stepping
+                self.ScalarTimeStepping.update()
+                self.VelocityTimeStepping.update()
+
+                #Update boundary conditions
+                self.ScalarState.boundary_exchange()
+                self.VelocityState.boundary_exchange()
+                self.ScalarState.update_all_bcs()
+                self.VelocityState.update_all_bcs()
+
+                #Call pressure solver
+                self.PSolver.update()
+        return
+
+
---
+++ main_n.py
@@ -0,0 +1,70 @@
+from mpi4py import MPI
+import argparse
+import json
+import numpy as np
+
+import SimulationClass
+
+def main(namelist):
+
+    n = 2
+    gcm_res = 50*1e3
+    couple_dt = 120.0
+    forced_fields = ['qv', 's']
+    domains = []
+    uls = 5.0
+    for i in range(n):
+        namelist["meta"]["output_directory"] = './couple_' + str(i)
+        domains.append(SimulationClass.Simulation(namelist, i))
+        domains[i].initialize()
+
+    ls_state = [] 
+    #Get GCM initial condition
+    for i in range(n): 
+        ls_state.append({})
+        for v in forced_fields:
+            ls_state[i][v] = domains[i].ScalarState.mean(v)
+            np.shape(ls_state[i][v])
+
+    #Set up storage for LES 
+    ls_forcing = []
+    ss_forcing = []
+    for i in range(n):
+        ss_forcing.append({})
+        ls_forcing.append({})
+        #les_tend.append({})
+
+    for couple_time in np.arange(couple_dt,21600.0+couple_dt, couple_dt):
+        print(couple_time)
+
+    for couple_time in np.arange(couple_dt,21600.0+couple_dt, couple_dt):
+        for i in range(n):
+            for v in forced_fields: 
+                ls_forcing[i][v] = (ls_state[i][v] - domains[i].ScalarState.mean(v) )/couple_dt
+            domains[i].update(couple_time, ls_forcing[i])
+            for v in forced_fields: 
+                ss_forcing[i][v] = (domains[i].ScalarState.mean(v) - ls_state[i][v])/couple_dt
+            
+        for i in range(n):
+            for v in forced_fields:
+                adv = uls * (ls_state[(i-1)%n][v] - ls_state[i%n][v] )/gcm_res
+                ls_state[i][v] += adv * couple_dt + ss_forcing[i][v] * couple_dt
+
+
+        for i in range(n):
+            print(ss_forcing[i]['qv'])
+
+
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Input for Columbia an LES!')
+    parser.add_argument('inputfile')
+    args = parser.parse_args()
+
+    with open(args.inputfile, 'r') as namelist_h:
+        namelist = json.load(namelist_h)
+        main(namelist)
+
+
