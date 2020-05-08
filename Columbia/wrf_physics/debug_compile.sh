eaf71ae158c043015fd89cf956dd9eaeb3681603
Kyle Pressel <pressel@gmail.com>
2020-05-08 16:02:10 -0700

    Adding compile script for warm_sbm.

--- Columbia/wrf_physics/debug_compile.sh
+++ Columbia/wrf_physics/debug_compile.sh
@@ -1,3 +1,3 @@
-#f2py -c module_mp_fast_sbm.F90 --verbose --opt='-fcheck=all -ftree-vectorize' -m module_mp_fast_sbm
+#f2py -c module_mp_fast_sbm.F90 --verbose --opt='-fdefault-real-8 -fcheck=all -ftree-vectorize' -m module_mp_fast_sbm
 
-f2py -c module_mp_fast_sbm.F90 --verbose  -m module_mp_fast_sbm
+f2py -c module_mp_fast_sbm_warm.F90 --verbose --opt='' -m module_mp_fast_sbm_warm
