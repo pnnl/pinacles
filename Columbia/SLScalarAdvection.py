9c3f30b091ec4f033d14daaf903fcfe04dfc17e1
Kyle Pressel <pressel@gmail.com>
2020-07-10 12:25:56 -0700

    Clean-up.

--- Columbia/SLScalarAdvection.py
+++ Columbia/SLScalarAdvection.py
@@ -135,31 +135,41 @@ def compute_SL_tend_bounded(phi, phi_t, dt, coeff_tensor1, coeff_tensor2, gamma=
     for i in range(1,shape[0]-1):
         for j in range(1,shape[1]-1):
             for k in range(1,shape[2]-1):
+                
+                #Initialize values
                 phi_old = phi[i,j,k]
                 phi_new = 0.0
-
                 phi_max = -1e9
                 phi_min = 1e9
-
                 first = 0.0
-                second = 0.0 
-                #print(np.sum(coeff_tensor[i,j,k,:]),np.sum(coeff_tensor2[i,j,k,:]))
+                second = 0.0
+
                 for p in range(3):
                     for r in range(3):
                         for s in range(3):
+
+                            #Grab the value of the scalar
                             phi_loc = phi[i+p-1,j+r-1,k+s-1]
+
+                            #CTU Interpolation stage
                             first += coeff_tensor1[i,j,k,p,r,s]*phi_loc
+
+                            #SL2 Interpolation stage
                             second += coeff_tensor2[i,j,k,p,r,s]*phi_loc
 
+                            #Compute the SL2 stencil bounds
                             phi_max = max(phi_max, phi_loc)
                             phi_min = min(phi_min, phi_loc)
 
+                #Convex combination of the CTU and SL2 schemes
                 phi_new = (1.0 - gamma)*first  +  gamma * second
-                #sgn = np.sign(phi_max - phi_new)
+
+                #Here we apply the correction to ensure boundedness
                 if phi_new > phi_max or phi_new < phi_min:
                     phi_new = first
-                phi_t[i,j,k] += (phi_new-phi_old)/dt
 
+                #Back out the tendencies and apply!
+                phi_t[i,j,k] += (phi_new-phi_old)/dt
 
 
 class CTU(ScalarAdvection.ScalarAdvectionBase):
@@ -168,39 +178,37 @@ class CTU(ScalarAdvection.ScalarAdvectionBase):
 
         ScalarAdvection.ScalarAdvectionBase.__init__(self, Grid, Ref, ScalarState, VelocityState, TimeStepping)
         coeff_shape = (self._Grid.ngrid_local[0], self._Grid.ngrid_local[1], self._Grid.ngrid_local[2],3,3,3)
+        
+        #Initialze two tensors for storing the coefficients
         self._coeff_tensor2 = np.zeros(coeff_shape, dtype=np.double, order='C')
         self._coeff_tensor1 = np.zeros(coeff_shape, dtype=np.double, order='C')
-        print(coeff_shape)
 
         return
 
 
     def update(self):
 
+        # Grab the velocities
         u = self._VelocityState.get_field('u')
         v = self._VelocityState.get_field('v')
         w = self._VelocityState.get_field('w')
 
+        # Grab the time an spatial resolution
         dt = self._TimeStepping.dt
         dxi = self._Grid.dxi
 
-
-        t0 = time.time() 
+        #Compute the coefficient tensors
         compute_coeff_tensor_biquad(u,v,w,dxi, dt, self._coeff_tensor2)
         compute_coeff_tensor(u,v,w,dxi, dt, self._coeff_tensor1)
-        t1 = time.time() 
-        print('Computing coefficients', t1 - t0)
-
 
         #Now iterate over the scalar variables
-        t0 = time.time()
         for var in self._ScalarState.names:
-
+            
+            #Grab the sclars and tendencies for each field
             phi = self._ScalarState.get_field(var)
             phi_t = self._ScalarState.get_tend(var)
 
             compute_SL_tend_bounded(phi, phi_t, dt, self._coeff_tensor1, self._coeff_tensor2)
             #compute_SL_tend(phi, phi_t, dt, self._coeff_tensor1)
-        t1  = time.time()
-        print('Scalar Update', t1-t0) 
+
         return
