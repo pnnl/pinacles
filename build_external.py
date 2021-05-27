278b9dc8e14a74a7ad444a66858d89d071ef8e8f
Pressel, Kyle G <kyle.pressel@pnnl.gov>
2021-05-27 19:49:03 +0000

    Kylepres/submod dat

--- build_external.py
+++ build_external.py
@@ -5,23 +5,6 @@ def main():
     build_script('pinacles/wrf_physics', 'build_p3.sh', 'p3')
     f2py_file('pinacles/wrf_physics/', 'module_mp_kessler.f95', 'kessler')
 
-    download_data('pinacles/wrf_physics/') 
-
-    return 
-
-def download_data(path):
-    urls = ['https://raw.githubusercontent.com/wrf-model/WRF/master/run/p3_lookup_table_1.dat-v4.1',
-            'https://raw.githubusercontent.com/wrf-model/WRF/master/run/p3_lookup_table_2.dat-v4.1']
-
-    orig_path = os.getcwd()
-    os.chdir(path)
-    
-    for u in urls:
-        cmd = 'wget ' + u 
-        subprocess.call([cmd], shell=True)
-
-    os.chdir(orig_path)
-    
     return 
 
 def f2py_file(path, source, extname):
