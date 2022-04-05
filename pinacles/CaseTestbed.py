fixed calculation of coriolis param when latitude changes in time

pinacles/CaseTestbed.py
@@ -645,7 +645,7 @@ class ForcingTestbed(Forcing.ForcingBase):
                 fill_value="extrapolate",
                 assume_sorted=True,
             )(current_time)
-            self._f = 2.0 * parameters.OMEGA * np.sin(current_lat[0] * np.pi / 180.0)
+            self._f = 2.0 * parameters.OMEGA * np.sin(current_lat * np.pi / 180.0)
 
 
         # interpolate in time